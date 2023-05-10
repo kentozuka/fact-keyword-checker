061544dfcb841340f5559ded0e7cb6b092df8c1a
Kento <kentozuka22@gmail.com>
2023-05-10 14:28:57 +0900

    initial

---
+++ tests/amazon.ts
@@ -0,0 +1,17 @@
+class SearchSuggest {
+  constructor() {}
+
+  async typeInWord() {
+    // types in the word in the search box
+  }
+
+  async getResult() {
+    // parse html and get the result
+  }
+
+  async upload() {
+    // uploads the data to the db
+  }
+}
+
+export default function getAmazonSuggest(query: string) {}
